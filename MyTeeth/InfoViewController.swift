904b3116edd67849540d09981fbe3389fcaa8468
id2k1149 <id2k1149@gmail.com>
2023-01-29 19:28:44 -0500

    add empty class InfoViewController

---
+++ MyTeeth/InfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InfoViewController.swift
+//  MyTeeth
+//
+//  Created by Max Franz Immelmann on 1/29/23.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
